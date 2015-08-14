150afadafbaca75c1b7cd67a8f5ae40113a8774e
jtate <jeremy.r.tate@gmail.com>
2015-08-14 17:48:01 -0400

    Initial commit

---
+++ src/DiceRoller.java
@@ -0,0 +1,14 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jtate on 8/14/15.
+ */
+public class DiceRoller extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
